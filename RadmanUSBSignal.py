83132c8e14c9fc4772d734e999a0c41d825b47be
JemmaRogers <jemma.rogers@offworld.ai>
2021-12-31 13:20:48 +0200

    Update RadmanUSBSignal.py

--- RadmanUSBSignal.py
+++ RadmanUSBSignal.py
@@ -3,8 +3,28 @@ import time
 import os
 import serial
 import serial.tools.list_ports
+from datetime import datetime
+import csv
+import psycopg2
+
 
 if __name__ == '__main__' :
+
+    timestamp = datetime.now()
+    timestampString = timestamp.strftime("%Y%m%d")
+    cf=open("Radman"+timestampString+".csv","x")
+    f = open("Radman"+timestampString+".csv","a")
+
+    #Establishing the connection
+    conn = psycopg2.connect(
+       database="postgres", user='admin', password='administrator', host='127.0.0.1', port= '5435'
+    )
+    #Setting auto commit false
+    conn.autocommit = True
+
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
     try :
         portlist = serial.tools.list_ports.comports()
 
@@ -25,14 +45,21 @@ if __name__ == '__main__' :
             time.sleep(.1)
             data3 = ser.readline()
             print(data3)
-            while True:
+            while True and not f.closed:
                 data = ser.readline()
                 time.sleep(.1)
                 data+= ser.read()
                 print(data)
+                csvwriter=csv.writer(f)
+                csvwriter.write(ser.readline)
+                cursor.execute('''INSERT INTO EMFRADMAN(ID, DATETIME, VALUE) VALUES (datetime.now(), ser.readline)''')
+
+                # Commit your changes in the database
+                conn.commit()
+                print("Records inserted........")
     except :
         print('Program exit !')
         pass
     finally :
-        #ser.close()
+        f.close()
         pass
