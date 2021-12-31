c9384406184c10aa386eccee014eb11a8acf5588
JemmaRogers <jemma.rogers@offworld.ai>
2021-12-31 13:21:31 +0200

    Update Syncing.py

--- Syncing.py
+++ Syncing.py
@@ -2,7 +2,8 @@ import snap7
 import ctypes
 from datetime import datetime
 import time
-
+import csv
+import psycopg2
 
 timestamp = datetime.now()
 timestampString = timestamp.strftime("%Y%m%d")
@@ -14,6 +15,17 @@ client.connect("192.168.10.2", 0, 0, 102)
 connected = client.get_connected()
 print(connected)
 
+#Establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='admin', password='administrator', host='127.0.0.1', port= '5435'
+)
+#Setting auto commit false
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+
 while(not f.closed):
 	if(connected):
 		size_to_read = 2646 #taken from TIA portal, offset size
@@ -21,6 +33,14 @@ while(not f.closed):
 		result = client.as_db_read(76, 522, size_to_read,data)
 		time.sleep(0.5)
 		print(result)
+		csvwriter=csv.writer(f)
+        csvwriter.write(result)
+        #THIS NEEDS TO BE TESTED
+        cursor.execute('''INSERT INTO RECIPELOGS(SEQNO, DATE, TIME, EXPNO,POSX,POSY,POSZ,USSR1,USSR2,USSR3,USSR4,MWAPPLIED,REFLECTEDPOWER,LEVELFORWARD,LEVELHIGHVOLT,ANODICCURRENT,METHANE,EXPNOTES) VALUES (result)''')
+
+        # Commit your changes in the database
+        conn.commit()
+        print("Records inserted........")
 	else:
 		f.close()
 	print(data)
